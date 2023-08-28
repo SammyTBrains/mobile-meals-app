625dcba178decc360aba95cee1e0e9da36b9875a
SammyTBrains <josephsamuel034@gmail.com>
2023-08-28 21:26:14 +0100

    navigations container

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <SafeAreaView>
-        <CategoriesScreen />
-      </SafeAreaView>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
