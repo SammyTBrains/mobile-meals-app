2ae5b9d1d6a14bec1699415037fb57f09af70835
SammyTBrains <josephsamuel034@gmail.com>
2023-08-28 21:13:45 +0100

    change

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <SafeAreaView>
+        <CategoriesScreen />
+      </SafeAreaView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
