badb78fd2ab3aa047996a927d063af86844b526b
SammyTBrains <josephsamuel034@gmail.com>
2023-08-29 22:44:06 +0100

    Drawer

--- App.tsx
+++ App.tsx
@@ -3,13 +3,37 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import { RootStackParamList } from "./type_utilities/types"; // Import the RootStackParamList type
 import MealDetailsScreen from "./screens/MealDetailsScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const Drawer = createDrawerNavigator<RootStackParamList>();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#9b5607" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#976b39" },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+        }}
+      />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+};
 
 export default function App() {
   return (
@@ -24,10 +48,10 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
-              title: "All Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
--- type_utilities/types.tsx
+++ type_utilities/types.tsx
@@ -1,6 +1,12 @@
 export type RootStackParamList = {
-  MealsCategories: undefined;
+  //Drawer Navigator nested in Native Stack Navigator
+  Drawer: undefined;
+  Categories: undefined;
+  Favorites: undefined;
+
+  //The rest in Native Stack Navigator
   MealsOverview: { categoryId: string };
   MealDetails: { mealId: string };
+
   // Add other screen names and their corresponding parameter types if needed
 };
